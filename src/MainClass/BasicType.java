9a53490602b4e407b56ad963b77d82f5408d5492
Nikita Mezenzew <neckitmez@jandex.com>
2015-05-24 23:02:34 +0300

    BasicType1

--- src/MainClass/BasicType.java
+++ src/MainClass/BasicType.java
@@ -6,30 +6,78 @@ public class BasicType {
 	int a;
 	Scanner sr;
 	void zadanie1 (){
-		for (int a=1000; a<=9999; a=a+3) {
-		if(a == 9999) {
-	    break;
-		}
-		System.out.print(a + " ");
-	}
-	System.out.print("Stop!");
+            System.out.println("Cosine:60 is equal to: 0.5");
+            System.out.println("Cosine:45 is equal to: 0.7071");
+            System.out.println("Cosine:40 is equal to: 0.766");
 	}
 	void zadanie2 (){
-		for (int a=1000; a<=9999; a=a+3) {
-		if(a == 9999) {
-	    break;
-		}
-		System.out.print(a + " ");
-	}
-	System.out.print("Stop!");
+            System.out.print("Enter the length of the first leg: ");
+            int a = sr.nextInt();
+            System.out.print("Enter the length of the second leg: ");
+            int b = sr.nextInt();
+            int c=(a*a)+(b*b);
+            int d= a+b+c;
+            System.out.println("Perimeter: " +d);
+            int e= (a*b)/2;
+            System.out.println("The area of a right triangle: " +e);
 	}
 	void zadanie3 (){
-		for (int a=1000; a<=9999; a=a+3) {
-		if(a == 9999) {
-	    break;
-		}
-		System.out.print(a + " ");
+            System.out.print("Enter the number: ");
+            int a = sr.nextInt();
+            int b = 0;
+            for ( ; a != 0 ; a /= 10)
+            ++b;
+            System.out.println("Figures including" +b);
 	}
-	System.out.print("Stop!");
+	void zadanie4 (){
+		System.out.println( (int)(Math.random()*7) - 3 );
+	}
+	void zadanie5 (){
+		System.out.print("Enter the number: ");
+        int a = sr.nextInt();
+        int b = a+a+1;
+		System.out.println( (int)(Math.random()*b) - a );
+	}
+	void zadanie6 (){
+		System.out.print("Enter the number: ");
+        int a = sr.nextInt();
+        int b = a+a+1;
+		System.out.println( (int)(Math.random()*b) - a );
+	}
+	void zadanie7 (){
+		System.out.print("Enter the number: ");
+        int a = sr.nextInt();
+        System.out.print("Enter the number: ");
+        int b = sr.nextInt();
+        if (a>b){
+        	System.out.println("Error!");
+        }
+        if (a<b){
+        	System.out.println( (int)(Math.random()*a+b) );
+        }
+	}
+	void zadanie8 (){
+		System.out.println((double)21/8); 
+	}
+	void zadanie9 (){
+		int n;
+		int sum = 0; 
+		System.out.print("Entering integer: ");
+		n = sr.nextInt();
+		do
+		sum = sum + (n % 10);
+		while((n/=10) != 0);
+		System.out.println("Sum of the numbers: " +sum );
+	}
+	void zadanie (){
+		int n;
+		int sum = 0; 
+		Scanner sr = new Scanner(System.in);
+		System.out.print("Entering integer: ");
+		n = sr.nextInt();
+		do
+		sum = sum + (n % 10);
+		while((n/=10) != 0);
+		System.out.println("Sum of the numbers: " +sum );
 	}
 }
--- src/MainClass/MainClass.java
+++ src/MainClass/MainClass.java
@@ -6,10 +6,26 @@ public class MainClass {
 	int a;
 	Scanner sr;
 	public static void main(String[] args) {
+		BasicType app1= new BasicType();
+		app1.zadanie1();
+		BasicType app2= new BasicType();
+		app2.zadanie2();
+		BasicType app3= new BasicType();
+		app3.zadanie3();
+		BasicType app4= new BasicType();
+		app4.zadanie4();
+		BasicType app5= new BasicType();
+		app5.zadanie5();
+		BasicType app6= new BasicType();
+		app6.zadanie6();
+		BasicType app7= new BasicType();
+		app7.zadanie7();
+		BasicType app8= new BasicType();
+		app8.zadanie8();
+		BasicType app9= new BasicType();
+		app9.zadanie9();
 		BasicType app= new BasicType();
-		app.zadanie1();
-		BasicType blalblab = new BasicType();
-		blalblab.zadanie1();
+		app.zadanie();
 	}
 		
 }
